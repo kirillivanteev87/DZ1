5ebc07dc52fc2d55ebc808c8a43683d5d2ded0a0
Kirill <ivantee87@mail.ru>
2022-04-18 23:12:41 +0300

    написана программа из двух чисел выбирает большее

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello world");
+﻿int numberA = -9;
+int numberB = -3;
+if (numberA > numberB)
+{
+    Console.Write("max =");
+    Console.WriteLine (numberA);
+}
+else 
+{
+    Console.Write("max = ");
+    Console.WriteLine(numberB);
+}
+
